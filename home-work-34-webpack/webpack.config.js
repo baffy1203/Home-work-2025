d01dd2ffd2f5b0a3be43deda4fe16f6f92a151b8
Viktoriia Fedenko <vika5364452@gmail.com>
2026-03-08 20:57:59 +0200

    HW 34 webpack

---
+++ home-work-34-webpack/webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    main:'./src/index.js',
+    stats:'./src/statistics.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.[contenthash].js'
+  },
+  plugins: [
+    new HTMLWebpackPlugin({ template: './src/index.html' }),
+    new CleanWebpackPlugin()
+  ],
+ 
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: [
+            'style-loader',
+            'css-loader'
+          ]
+        },
+        {
+          test: /\.(png|jpg|jpeg|svg|gif|webp)$/,
+          type: 'asset/resource'
+        }
+      ]
+    }
+  };
+
